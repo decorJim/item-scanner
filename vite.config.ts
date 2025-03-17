4bfa2c4307c7c646563a760d3d98621cf764e4da
Victor Kim <victorkim@hotmail.ca>
2025-03-17 16:14:55 -0400

    add base property in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/item-scanner/', // add for github pages
   plugins: [react()],
   build: {
     outDir: 'build', // This ensures that the output is directed to the 'build' folder
