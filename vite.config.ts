ba94ac9a0df1629247da9aeb106108b92b7e8ad7
Victor Kim <victorkim@hotmail.ca>
2025-03-17 00:08:46 -0400

    few changes on build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // This ensures that the output is directed to the 'build' folder
+  },
 })
