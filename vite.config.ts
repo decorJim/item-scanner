cfd164a6f12d506f1ec6f1424c60a2e6fa337ce5
Victor Kim <victorkim@hotmail.ca>
2025-03-17 16:25:14 -0400

    use relative path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/item-scanner/', // add for github pages
+  base: './', // force vite to use relative path
   plugins: [react()],
   build: {
     outDir: 'build', // This ensures that the output is directed to the 'build' folder
